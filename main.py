0489142d8afc182ea69f376158e4362eace31a2c
Ahmad Fauor <ahmadfuor@gmail.com>
2026-03-31 18:36:44 +0300

    create first home screen UI

--- main.py
+++ main.py
@@ -1,9 +1,51 @@
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+
+
+
 
 class FauorApp(App):
     def build(self):
-        return Label(text='Welcome to Fauor Market!')
+       main_layout = BoxLayout(
+           orientation='vertical',
+        padding =20,
+        spacing = 15
+        ) 
+       title_label = Label(
+           text='פאעור מרקט',
+           font_size=32,
+       )
+       welcome_label = Label(
+           text='ברוכים הבאים לפאעור מרקט',
+           font_size=20,
+       )
+       products_button = Button(
+           text='צפיה במוצרים',
+           size_hint=(1, 0.2)
+       )
+       cart_button = Button(
+           text = 'הסל שלי ',
+           size_hint=(1, 0.2)    
+       )
+       exit_button = Button(
+              text = 'יציאה',
+              size_hint=(1, 0.2)
+       )
+       
+       exit_button.bind(on_press=self.close_app)
+
+       main_layout.add_widget(title_label)
+       main_layout.add_widget(welcome_label)
+       main_layout.add_widget(products_button)
+       main_layout.add_widget(cart_button)
+       main_layout.add_widget(exit_button)
+
+       return main_layout
+def close_app(self, instance):
+    App.get_running_app().stop()
+    
 if __name__ == '__main__':
     FauorApp().run()
       
