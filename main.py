7601d6ed7a6bfb96e0b5119e0b8d04483e45ed03
Ahmad Fauor <ahmadfuor@gmail.com>
2026-03-31 17:44:27 +0300

    setup virtual environment and install kivy

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("welcome to Fauor Market App")
+from kivy.app import App
+from kivy.uix.label import Label
+
+class FauorApp(App):
+    def build(self):
+        return Label(text='Welcome to Fauor Market!')
+if __name__ == '__main__':
+    FauorApp().run()
+      
