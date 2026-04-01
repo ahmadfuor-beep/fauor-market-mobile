80d7aaddbf644b29a22526135d0be424b82027a6
Ahmad Fauor <ahmadfuor@gmail.com>
2026-04-01 23:03:23 +0300

    add screen manager and products screen

--- main.py
+++ main.py
@@ -2,49 +2,106 @@ from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
 
 
-class FauorApp(App):
-    def build(self):
-       main_layout = BoxLayout(
+class HomeScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(
            orientation='vertical',
         padding =20,
         spacing = 15
         ) 
-       title_label = Label(
+        title_label = Label(
            text='Fauor Market',
            font_size=32
        )
-       welcome_label = Label(
+        welcome_label = Label(
            text='Welcome to Fauor Market!',
            font_size=20
        )
-       products_button = Button(
+        products_button = Button(
            text='View Products',
            size_hint=(1, 0.2)
        )
-       cart_button = Button(
+        cart_button = Button(
            text = 'My Cart',
            size_hint=(1, 0.2)    
        )
-       exit_button = Button(
+        exit_button = Button(
               text = 'Exit',
               size_hint=(1, 0.2)
        )
        
-       exit_button.bind(on_press=self.close_app)
+        products_button.bind(on_press=self.go_to_products)
+        exit_button.bind(on_press=self.close_app)
 
-       main_layout.add_widget(title_label)
-       main_layout.add_widget(welcome_label)
-       main_layout.add_widget(products_button)
-       main_layout.add_widget(cart_button)
-       main_layout.add_widget(exit_button)
+        layout.add_widget(title_label)
+        layout.add_widget(welcome_label)
+        layout.add_widget(products_button)
+        layout.add_widget(cart_button)
+        layout.add_widget(exit_button)
+       
+        self.add_widget(layout)
+        #function to switch to products screen
+    def go_to_products(self, instance):
+        # Switch to the products screen
+        self.manager.current = "products"
 
-       return main_layout
     def close_app(self, instance):
-     App.get_running_app().stop()
+        App.get_running_app().stop()
+
+class ProductsScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(
+            orientation="vertical",
+            padding=20,
+            spacing=15
+        )
+
+        title_label = Label(
+            text="Products",
+            font_size=30
+        )
+
+        product_1 = Label(text="Milk - 5.90 NIS")
+        product_2 = Label(text="Bread - 7.50 NIS")
+        product_3 = Label(text="Eggs - 12.00 NIS")
+
+        back_button = Button(
+            text="Back to Home",
+            size_hint=(1, 0.2)
+        )
+
+        back_button.bind(on_press=self.go_back)
+
+        layout.add_widget(title_label)
+        layout.add_widget(product_1)
+        layout.add_widget(product_2)
+        layout.add_widget(product_3)
+        layout.add_widget(back_button)
+
+        self.add_widget(layout)
+
+    def go_back(self, instance):
+        self.manager.current = "home"
+
+
+class FauorApp(App):
+    def build(self):
+        screen_manager = ScreenManager()
+
+        screen_manager.add_widget(HomeScreen(name="home"))
+        screen_manager.add_widget(ProductsScreen(name="products"))
+
+        return screen_manager
+
     
 if __name__ == "__main__":
     FauorApp().run()
