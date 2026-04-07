0e19bf4f04950fcaeb97464b4880777405a3abcb
Ahmad Fauor <ahmadfuor@gmail.com>
2026-04-07 19:27:25 +0300

    add add-to-cart functionality

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ def load_products():
 
     with open(file_path, "r", encoding="utf-8") as file:
         return json.load(file)
+# cart to store selected products
+cart = []
 
 class HomeScreen(Screen):
     def __init__(self, **kwargs):
@@ -69,7 +71,7 @@ class ProductsScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        layout = BoxLayout(
+        self.layout = BoxLayout(
             orientation="vertical",
             padding=20,
             spacing=15
@@ -79,23 +81,48 @@ class ProductsScreen(Screen):
             text="Products",
             font_size=30
         )
-        layout.add_widget(title_label)
+        self.layout.add_widget(title_label)
 
-        products = load_products()
+        self.load_products_ui()
         
-        for product in products:
-            product_label = Label(text=f"{product['name']} - {product['price']:.2f} NIS")
-            layout.add_widget(product_label)
-
         back_button = Button(
             text="Back to Home",
             size_hint=(1, 0.2)
         )
         
         back_button.bind(on_press=self.go_back)
-        layout.add_widget(back_button)
+        self.layout.add_widget(back_button)
 
-        self.add_widget(layout)
+        self.add_widget(self.layout)
+        
+
+    def load_products_ui(self):
+        products = load_products()
+
+        for product in products:
+            product_box = BoxLayout(size_hint=(1, 0.2))
+
+            product_label = Label(
+                text=f"{product['name']} - {product['price']} NIS"
+            )
+
+            add_button = Button(
+                text="Add",
+                size_hint=(0.3, 1)
+            )
+
+            add_button.bind(
+                on_press=lambda instance, p=product: self.add_to_cart(p)
+            )
+
+            product_box.add_widget(product_label)
+            product_box.add_widget(add_button)
+
+            self.layout.add_widget(product_box)
+
+    def add_to_cart(self, product):
+        cart.append(product)
+        print(f"Added to cart: {product['name']}")
 
     def go_back(self, instance):
         self.manager.current = "home"
